5cdd5688451fcbb1a2ebe10290a263e6e85da2b3
shsrivastava754 <shsrivastava@paymentus.com>
2023-03-13 10:27:06 +0530

    Added port to .env

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 3000;
 const webPush = require('web-push');
 const path = require('path');
-
 const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
+const public_vapid_key = process.env.PUBLIC_KEY;
+const private_vapid_key = process.env.PRIVATE_KEY;
+
 app.use(bodyParser.json());
 
 // Middleware to use client folder
 app.use(express.static(path.join(__dirname,"client")));
 
-const public_vapid_key = process.env.PUBLIC_KEY;
-const private_vapid_key = process.env.PRIVATE_KEY;
 
 webPush.setVapidDetails('mailto:test@test.com',public_vapid_key,private_vapid_key);
 
